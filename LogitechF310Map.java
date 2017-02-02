6a63be01df8b5acbbd844651af177ed07c8775af
javadotmakeitwork <njdonahue17@gmail.com>
2017-02-01 19:16:29 -0500

    Create LogitechF310Map.java

---
+++ LogitechF310Map.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team4028.robot;
+
+public class LogitechF310Map  {
+	public static final int A_BUTTON = 1;
+	public static final int B_BUTTON = 2;
+	public static final int X_BUTTON = 3;
+	public static final int Y_BUTTON = 4;
+	public static final int LEFT_BUTTON = 5;
+	public static final int RIGHT_BUTTON = 6;
+	public static final int BACK_BUTTON = 7;
+	public static final int START_BUTTON = 8;
+	public static final int LEFT_THUMBSTICK_PRESS = 9;
+	public static final int RIGHT_THUMBSTICK_PRESS = 10;
+	
+	public static final int LEFT_THUMBSTICK_X = 0;
+	public static final int LEFT_THUMBSTICK_Y = 1;
+	public static final int LEFT_TRIGGER = 2;
+	public static final int RIGHT_TRIGGER = 3;
+	public static final int RIGHT_THUMBSTICK_X = 4;
+	public static final int RIGHT_THUMBSTICK_Y = 5;
+	
+
+}
